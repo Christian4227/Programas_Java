b9126a95cce811bfc63f7345885c113085ee8c61
ChristianSCubo <67664694+Christian4227@users.noreply.github.com>
2021-09-13 09:55:37 -0300

    Novos arquivos Java
    Atualizando os arquivos Java existentes no repositório

--- Atividade_3/Circunferencia.java
+++ Atividade_3/Circunferencia.java
@@ -1,11 +1,26 @@
 package Atividade_3;
 import java.util.Scanner;
 
-public class Circunferencia {
-	public int getRaio() {
-		System.out.println("Digite um valor inteiro para o raio.");
-		Scanner entrada = new Scanner(System.in);
-		Integer raio = entrada.nextInt();
+class Circunferencia {
+	public double raio;
+	
+	public void setRaio(double r) {
+		if (r >= 0)
+			raio = r;
+		else {
+			raio = 0;
+		}
+	}
+	
+	public double getRaio() {
 		return raio;
+	}
+	
+	public double getArea(double raio) {
+		return Math.pow(raio, 2);
+	}
+	
+	public double getPerimetro(double raio) {
+		return 2 * 3.14 * raio;
 	}
 }
--- Atividade_3/Ex01.java
+++ Atividade_3/Ex01.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 
 public class Ex01 {
 	public static void main(String args[]) {
-		Circunferencia raio = new Circunferencia();
-		Integer vraio = raio.getRaio();
-		Double perimetro = 2 * 3.14 * vraio;
-		Double area = 3.14 * Math.pow(vraio, 2);
-		Integer diametro = 2 * vraio;	
-		System.out.println("Diâmetro: " + diametro);
-		System.out.printf("Perímetro: %.1f \n", perimetro);
-		System.out.println("Área: " + area);
+		Circunferencia circulo = new Circunferencia();
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Digite o valor do raio com uma casa decimal.");
+		Double raio = entrada.nextDouble();
+		
+		System.out.printf("O valor da área da circunferência é: %.2f\n", circulo.getArea(raio));
+		System.out.printf("O valor do perímetro da circunferência é: %.2f\n", circulo.getPerimetro(raio));
 	}
 }
